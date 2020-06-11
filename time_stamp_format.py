1bb9499eeed94f94a81acf8a78d53603982afb79
Nic La <sakeofmaking@gmail.com>
2020-06-10 18:59:28 -0700

    Initial commit

---
+++ time_stamp_format.py
@@ -0,0 +1,31 @@
+"""
+Time Stamp Format
+
+Description: Take time stamp of format #h#m#s#ms and reformat as ##:##:##:###
+
+Author: Nic La
+Last modified: Jun 2020
+"""
+
+
+import csv
+import re
+
+
+# Open csv file
+# TODO: note, file separated via ';'
+# TODO: note, row 1 - 20 contain useless data
+# TODO: note, row 21 contains header
+# TODO: note, row 22 contains no data
+# TODO: note, first column contains timestamp
+# load csv file contents into list (may exceed RAM limits)
+
+
+# Use regex to extract h, m, s, ms from column 1 data
+
+
+# Replace column 1 data with new format
+
+
+# Return all data to new csv file
+

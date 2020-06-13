d5c45ad1b1244834300be55ecaa28017ec3bd7bf
Nic La <sakeofmaking@gmail.com>
2020-06-13 15:36:44 -0700

    Brought to a working state
    Passes the smoke test

--- time_stamp_format.py
+++ time_stamp_format.py
@@ -12,20 +12,56 @@ import csv
 import re
 
 
+# Initialize variables
+original_file = 'example_short.txt'
+new_file = 'example_new.txt'
+raw = []
+clean_time = []
+pattern_h = r'(\d{1,3})h'
+pattern_m = r'(\d{1,3})m(?!s)'
+pattern_s = r'(\d{1,3})s'
+pattern_ms = r'(\d{1,3})ms'
+
+
 # Open csv file
-# TODO: note, file separated via ';'
-# TODO: note, row 1 - 20 contain useless data
-# TODO: note, row 21 contains header
-# TODO: note, row 22 contains no data
-# TODO: note, first column contains timestamp
+# file separated via ';'
+# row 1 - 19 contain irrelevant data
+# row 20 contains header
+# row 21 contains no data
+# first column contains timestamp
 # load csv file contents into list (may exceed RAM limits)
+with open(original_file) as in_file:
+    reader = csv.reader(in_file, delimiter=';')
+    for row in reader:
+        raw.append(row)
 
 
-# Use regex to extract h, m, s, ms from column 1 data
-
-
-# Replace column 1 data with new format
+# Use regex to replace h, m, s, ms in column 1 data
+for line in raw[21:]:
+    match_h = re.search(pattern_h, line[0])
+    match_m = re.search(pattern_m, line[0])
+    match_s = re.search(pattern_s, line[0])
+    match_ms = re.search(pattern_ms, line[0])
+    if match_h is None:
+        h = 0
+    else:
+        h = match_h[1]
+    if match_m is None:
+        m = 0
+    else:
+        m = match_m[1]
+    if match_s is None:
+        s = 0
+    else:
+        s = match_s[1]
+    if match_ms is None:
+        ms = 0
+    else:
+        ms = match_ms[1]
+    line[0] = str(h) + ':' + str(m) + ':' + str(s) + ':' + str(ms)
 
 
 # Return all data to new csv file
-
+with open(new_file, 'w') as out_file:
+    writer = csv.writer(out_file, delimiter=';')
+    writer.writerows(raw)
